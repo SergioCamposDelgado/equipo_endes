53e581e22924d1f2383a77e9e9b720f010c6ab5e
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-03-25 12:50:10 +0100

    Generado el javadoc

--- code/equipo_trabajo/src/main/java/com/endes/entidad/ResponsablePruebas.java
+++ code/equipo_trabajo/src/main/java/com/endes/entidad/ResponsablePruebas.java
@@ -1,17 +1,37 @@
 package com.endes.entidad;
 
+/**
+ * @author Sergio Campos Delgado
+ */
 public class ResponsablePruebas extends Personal {
     private int bugsDetectados;
 
+    /**
+     * Crear un responsable de pruebas
+     * @param dni el dni del responsable
+     * @param nombre el nombre del responsable
+     * @param sueldoBase el sueldo del responsable
+     * @param bugsDetectados la cantidad de bugs detectados
+     * @throws IllegalArgumentException si alguno de los parametros no son validos
+     */
     public ResponsablePruebas(String dni, String nombre, double sueldoBase, int bugsDetectados) {
         super(dni, nombre, sueldoBase);
         setBugsDetectados(bugsDetectados);
     }
 
+    /**
+     * Obtiene el numero de bugs detectados
+     * @return el numero de bugs detectados
+     */
     public int getBugsDetectados() {
         return bugsDetectados;
     }
 
+    /**
+     * Pone el numero de bugs detectados
+     * @param bugsDetectados
+     * @throws IllegalArgumentException si los bugs son negativos
+     */
     public void setBugsDetectados(int bugsDetectados) {
         if (bugsDetectados < 0) {
             throw new IllegalArgumentException("El número de bugs detectados no puede ser negativo.");
@@ -19,6 +39,9 @@ public class ResponsablePruebas extends Personal {
         this.bugsDetectados = bugsDetectados;
     }
    
+    /**
+     * Calcula la profuctividad en base a los bugs detectados
+     */
     @Override
     public double calcularProductividad() {
         return getSueldoBase() * (1 + 0.02 * bugsDetectados);
