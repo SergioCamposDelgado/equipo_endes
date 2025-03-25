0cee6224b603ef614d749051528fc2fe551a51e3
Sergio Campos Delgado <campos.delgado.sergio@iescamas.es>
2025-03-25 12:43:22 +0100

    Añadidos test del constructor

--- code/equipo_trabajo/src/test/java/com/endes/entidad/PersonalTest.java
+++ code/equipo_trabajo/src/test/java/com/endes/entidad/PersonalTest.java
@@ -14,6 +14,59 @@ class PersonalTest {
 		p = new Desarrollador("11111111H", "Sergio", 1000.00, "Java");
 	}
 	
+    @Test
+    @DisplayName("No se permite crear una persona con DNI nulo")
+    void testContructorExcepcionDNINulo() {
+
+		Exception ex = assertThrows(IllegalArgumentException.class, ()->new Desarrollador(null,"Sergio", 1000.00, "Java") );
+		String mensajeEsperado = "DNI no válido.";
+		
+		assertEquals(mensajeEsperado, ex.getMessage());
+    }
+    
+    @Test
+    @DisplayName("No se permite crear una persona con DNI vacio")
+    void testContructorExcepcionDNIVacio() {
+		
+		Desarrollador d = null;
+		Exception ex = assertThrows(IllegalArgumentException.class, ()->new Desarrollador(" ","Sergio", 1000.00, "Java") );
+		String mensajeEsperado = "DNI no válido.";
+		
+		assertEquals(mensajeEsperado, ex.getMessage());
+    }
+    
+    @Test
+    @DisplayName("No se permite crear una persona con Nombre nulo")
+    void testContructorExcepcionNombreNulo() {
+
+		Exception ex = assertThrows(IllegalArgumentException.class, ()->new Desarrollador("11111111H",null, 1000.00, "Java") );
+		String mensajeEsperado = "Nombre no válido.";
+		
+		assertEquals(mensajeEsperado, ex.getMessage());
+    }
+    
+    @Test
+    @DisplayName("No se permite crear una persona con Nombre vacio")
+    void testContructorExcepcionNombreVacio() {
+
+		Exception ex = assertThrows(IllegalArgumentException.class, ()->new Desarrollador("11111111H"," ", 1000.00, "Java") );
+		String mensajeEsperado = "Nombre no válido.";
+		
+		assertEquals(mensajeEsperado, ex.getMessage());
+    }
+    
+    @Test
+    @DisplayName("No se permite crear una persona con sueldo negativo")
+    void testContructorExcepcionSueldoNegativo() {
+
+		Exception ex = assertThrows(IllegalArgumentException.class, ()->new Desarrollador("11111111H","Sergio", -1000.00, "Java") );
+		String mensajeEsperado = "El sueldo base no puede ser negativo.";
+		
+		assertEquals(mensajeEsperado, ex.getMessage());
+    }
+    
+
+	
 	@DisplayName("Test de metodo getDNI()")
 	@Test
 	void testGetDNI() {
